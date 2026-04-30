379ab63df5df83430e8525ebff46ee97ac2698a8
phemelo-ds <pshuping88@gmail.com>
2026-04-30 16:52:08 +0200

    Implement JSON persistence, menu navigation (match-case), and stable item entry loop
    -introduced persistent storage (JSON load/save)
    -redesigned control flow (menu + match)
    -fixed loop logic (clean infinite loop pattern)
    -stabilised data entry (safe append pattern)
    -removed earlier structural bugs

--- Projects/Grocery System/Grocery System v1.py
+++ Projects/Grocery System/Grocery System v1.py
@@ -9,20 +9,35 @@ try:
 except FileNotFoundError:
     grocery_list = []
 
+#Main Loop
+while True:
+    #Display CLI menu
+    print(f"-----GROCERY DATA TRACKER-----\n1. Add Item\n2. View Items\n3.Total Spend\n4.Exit Tracker")
+    user_choice = int(input("Enter Number: "))
 
-#control
-x = "1"
+    match user_choice:
+        case 1:
+            #Add item loop
+            while True:
+                loop_choice = int(input("Enter 0 to Continue/ 1 to Exit: "))
+                if loop_choice != 0:
+                    break
 
-#main loop
-while x == "1":
-    x = input("Enter 1 to continue or 0 to exit: \n")
-    if x != "1":
-        break
-    else:
-        grocery_item = {"Item Name":input("Enter item Name: "),"Price":float(input("Enter item Price: ")),"Quantity":int(input("Enter item Quantity: ")),"Date":input("Enter today's date (DD/MM/YY): "),"Category":input("Enter item Category: "),"Store":input("Enter store Name: ")}
-        grocery_list.append(grocery_item)
+                grocery_item = {"Item Name":input("Enter item Name: "),"Price":float(input("Enter item Price: ")),"Quantity":int(input("Enter item Quantity: ")),"Date":input("Enter today's date (DD/MM/YY): "),"Category":input("Enter item Category: "),"Store":input("Enter store Name: ")}
+                grocery_list.append(grocery_item)
+                print("Item succesfully added!")
 
-with open("grocery_data.json", "w") as file:
-    json.dump(grocery_list, file, indent=4)
-    
-print("Data saved successfully.")
+            with open("grocery_data.json","w") as file:
+                json.dump(grocery_list, file, indent = 4)
+            print("Data successfully saved to Memory!")
+
+        case 2:
+            print("View Items")
+        case 3:
+            print("Total Spend")
+        case 4:
+            print("Exiting Tracker!")
+            break
+        case _:
+            print("Wrong Value entered")
+
