d0afcf2dd5e980581000401a092df76f79777c78
phemelo-ds <pshuping88@gmail.com>
2026-04-30 12:20:06 +0200

    Updated Main Loop in Grocery System
    -Removed redundant code
    -Added Category's, Store, Date to main loop

--- Projects/Grocery System/Grocery System v1.py
+++ Projects/Grocery System/Grocery System v1.py
@@ -12,27 +12,7 @@ while x == "1":
     if x != "1":
         break
     else:
-        grocery_item = {"Name":input("Enter item Name: "),"Price":input("Enter item Price: "),"Quantity":input("Enter item Quantity: ")}
+        grocery_item = {"Item Name":input("Enter item Name: "),"Price":input("Enter item Price: "),"Quantity":input("Enter item Quantity: "),"Date":input("Enter today's date (DD/MM/YY): "),"Category":input("Enter item Category: "),"Store":input("Enter store Name: ")}
         grocery_list.append(grocery_item)
 
 print(grocery_list)
-
-#Grocery System v1
-
-#List to hold items
-grocery_list = []
-
-#control
-x = "1"
-
-#main loop
-while x == "1":
-    x = input("Enter 1 to continue or 0 to exit: \n")
-    if x != "1":
-        break
-    else:
-        grocery_item = {"Name":input("Enter item Name: "),"Price":input("Enter item Price: "),"Quantity":input("Enter item Quantity: ")}
-        grocery_list.append(grocery_item)
-
-print(grocery_list)
-
