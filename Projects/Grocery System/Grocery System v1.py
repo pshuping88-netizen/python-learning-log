f3b1d8998ff6b3b55c8b0fee24dd0ef51caf4ec8
phemelo-ds <pshuping88@gmail.com>
2026-04-30 15:18:55 +0200

    Added json file to grocery system
    - Checks for file
    - creates file
    -opens and closes file

--- Projects/Grocery System/Grocery System v1.py
+++ Projects/Grocery System/Grocery System v1.py
@@ -1,7 +1,14 @@
 #Grocery System v1
+#import module
+import json
 
-#List to hold items
-grocery_list = []
+#Try to open file if not found, creates new list
+try:
+    with open("grocery_data.json","r") as file:
+        grocery_list = json.load(file)
+except FileNotFoundError:
+    grocery_list = []
+
 
 #control
 x = "1"
@@ -12,7 +19,10 @@ while x == "1":
     if x != "1":
         break
     else:
-        grocery_item = {"Item Name":input("Enter item Name: "),"Price":input("Enter item Price: "),"Quantity":input("Enter item Quantity: "),"Date":input("Enter today's date (DD/MM/YY): "),"Category":input("Enter item Category: "),"Store":input("Enter store Name: ")}
+        grocery_item = {"Item Name":input("Enter item Name: "),"Price":float(input("Enter item Price: ")),"Quantity":int(input("Enter item Quantity: ")),"Date":input("Enter today's date (DD/MM/YY): "),"Category":input("Enter item Category: "),"Store":input("Enter store Name: ")}
         grocery_list.append(grocery_item)
 
-print(grocery_list)
+with open("grocery_data.json", "w") as file:
+    json.dump(grocery_list, file, indent=4)
+    
+print("Data saved successfully.")
