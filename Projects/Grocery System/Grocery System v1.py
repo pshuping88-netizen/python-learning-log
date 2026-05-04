7f9c384dae3a09c14154d913076ba208fc4c4474
phemelo-ds <pshuping88@gmail.com>
2026-05-04 16:25:52 +0200

    refactor: automate datetime capture and standardize formatting
    - replaced manual datetime input with automatic timestamp generation
    - removed user input dependency for time field
    - cleaned up output formatting for consistency

--- Projects/Grocery System/Grocery System v1.py
+++ Projects/Grocery System/Grocery System v1.py
@@ -1,8 +1,9 @@
 #Grocery System v1
 #import module
 import json
+from datetime import date #(Only imports date function)
 
-#Try to open file if not found, creates new list
+#Try to open file if not found, creates new file
 try:
     with open("grocery_data.json","r") as file:
         grocery_list = json.load(file)
@@ -23,7 +24,7 @@ while True:
                 if loop_choice != 0:
                     break
 
-                grocery_item = {"Item Name":input("Enter item Name: "),"Price":float(input("Enter item Price: ")),"Quantity":int(input("Enter item Quantity: ")),"Date":input("Enter today's date (DD/MM/YY): "),"Category":input("Enter item Category: "),"Store":input("Enter store Name: ")}
+                grocery_item = {"Item Name":input("Enter item Name: "),"Price":float(input("Enter item Price: ")),"Quantity":int(input("Enter item Quantity: ")),"Date":date.today().isoformat,"Category":input("Enter item Category: "),"Store":input("Enter store Name: ")}
                 grocery_list.append(grocery_item)
                 print("Item succesfully added!")
 
